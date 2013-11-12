de993ed75b1740cd843a1f8592e9299bb3422935
shuji <watanabe.shuji@classmethod.jp>
2013-11-12 09:53:57 +0900

    完了済みのタスクを削除する

--- todo/js/controllers/todos_controller.js
+++ todo/js/controllers/todos_controller.js
@@ -8,6 +8,14 @@ Todos.TodosController = Ember.ArrayController.extend({
     return remaining === 1 ? 'item' : 'items';
   }.property('remaining'),	
   
+  hasCompleted: function () {
+    return this.get('completed') > 0;
+  }.property('completed'),
+  
+  completed: function () {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+  
   actions: {
     createTodo: function () {
       // Get the todo title set by the "New Todo" text field
@@ -25,6 +33,13 @@ Todos.TodosController = Ember.ArrayController.extend({
 
       // Save the new model
       todo.save();
-    }
+    },
+    clearCompleted: function () {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    }    
   }
 });
+
+
